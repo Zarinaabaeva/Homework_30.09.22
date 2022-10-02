5bfd30361763ea0a2ca6d76b148133b18f00bf12
Zarina <abaevazarina@yandex.ru>
2022-10-02 22:22:58 +0300

    Task66 solved

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumIntervalNumbers(int M, int N)
+{
+    if (M > N)
+    {
+        return 0;
+    }
+    return M + SumIntervalNumbers(M + 1, N);
+}
+
+int M = Prompt("Введите число M: ");
+int N = Prompt("Введите число N: ");
+Console.Write(SumIntervalNumbers(M, N));

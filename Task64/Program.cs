8f36a8e479af22b3ea8cdde38d113da3c8f2e035
Zarina <abaevazarina@yandex.ru>
2022-10-02 22:18:39 +0300

    Task64 solved

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int IntervalNumbers(int N)
+{
+    Console.WriteLine(N);
+
+    if (N <= 1)
+    {
+        return 1;
+    }
+    N--;
+    return IntervalNumbers(N);
+}
+
+int N = Prompt("Введите число: ");
+IntervalNumbers(N);

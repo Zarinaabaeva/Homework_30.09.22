23a2c34dbe82e00891ec6838e8cccbb7e2845266
Zarina <abaevazarina@yandex.ru>
2022-10-02 22:35:41 +0300

    Task68 solved

---
+++ Task68/Program.cs
@@ -0,0 +1,20 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AkkermanFunction(int m, int n)
+{
+  if (m == 0)
+    return n + 1;
+  else
+    if ((m != 0) && (n == 0))
+      return AkkermanFunction(m - 1, 1);
+    else
+      return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+}
+
+int m = Prompt("Введите значение m: ");
+int n = Prompt("Введите значение n: ");
+Console.Write(AkkermanFunction(m, n));
